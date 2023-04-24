a7fbb5ed06cf3166763d6dccf3f27ef101674427
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-04-24 22:56:31 +0300

    add callBack in UncontrolledRating,UncontrolledAccordion, Rating, Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import {OnOff} from "./components/OnOff/OnOff";
 import {OnOff2} from "./components/OnOff(ver2)/OnOff2";
 import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
 import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 
-
-
 function App() {
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
     console.log("App rendering")
     return (
         <div>
@@ -18,34 +18,24 @@ function App() {
             <PageTitle title={"My friends"}/>
             Article 1
             <Rating value={3}/>*/}
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
-         <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
-            <OnOff />
-
-            <OnOff2 />
-            <OnOff2 />
-
-            <UncontrolledAccordion titleValue={"Menu"} />
-            <UncontrolledAccordion titleValue={"Users"} />
-
-            <UncontrolledRating />
-
-
-
-
-
+            <Accordion titleValue={"Menu"} collapsed={accordionCollapsed} onChange={()=>setAccordionCollapsed(!accordionCollapsed)}/>
+            <Accordion titleValue={"Users"} collapsed={accordionCollapsed} onChange={()=>setAccordionCollapsed(!accordionCollapsed)}/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <OnOff/>
+            <OnOff2/>
+            <OnOff2/>
+            <UncontrolledAccordion titleValue={"Menu"}/>
+            <UncontrolledAccordion titleValue={"Users"}/>
+            <UncontrolledRating/>
         </div>
     );
 }
-type PageTitlePropsType ={
-    title:string
+
+type PageTitlePropsType = {
+    title: string
 }
+
 function PageTitle(props: PageTitlePropsType) {
 
     console.log("PageTitle rendered")
--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -4,9 +4,6 @@ type AccordionPropsType = {
     titleValue: string
     // collapsed: boolean
 }
-type AccordionTitlePropsType = {
-    title: string
-}
 
 function UncontrolledAccordion(props: AccordionPropsType) {
     console.log("Accordion rendering")
@@ -14,18 +11,23 @@ function UncontrolledAccordion(props: AccordionPropsType) {
     let [collapsed, setCollapsed] = useState(false)
 
     return (<div>
-
-
-        <AccordionTitle title={props.titleValue}/>
-        <button onClick={() => {setCollapsed(!collapsed)}}>TOGGLE</button>
+        <AccordionTitle title={props.titleValue} onClick={() => {setCollapsed(!collapsed)}} />
+       {/* <button onClick={() => {setCollapsed(!collapsed)}}>TOGGLE</button>*/}
        {!collapsed && <AccordionBody/>}
     </div>)
 }
 
+type AccordionTitlePropsType = {
+    title: string
+    onClick: ()=>void
+}
+
 function AccordionTitle(props: AccordionTitlePropsType) {
     console.log("AccordionTitle rendering")
-    return (
-        <h3>---{props.title}---</h3>
+    return ( <>
+            <h3 onClick={()=> props.onClick()}>---{props.title}--- </h3>
+    </>
+
     )
 }
 
